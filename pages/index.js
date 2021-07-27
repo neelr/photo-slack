d0723908f2a1435554bffdbe2ff9c450ed360fc8
Claire Wang <41759033+ClaireBookworm@users.noreply.github.com>
2021-07-27 23:28:19 +0000

    styling

--- pages/index.js
+++ pages/index.js
@@ -10,24 +10,41 @@ const pee = (props) => {
                 display: -webkit-box; /* Not needed if autoprefixing */
                 display: -ms-flexbox; /* Not needed if autoprefixing */
                 display: flex;
-                margin-left: -30px; /* gutter size offset */
+                margin-left: -15px; /* gutter size offset */
+                margin-right: 15px;
                 width: auto;
               }
               .my-masonry-grid_column {
-                padding-left: 30px; /* gutter size */
+                padding-left: 2rem; /* gutter size */
                 background-clip: padding-box;
               }
               
               /* Style your items */
               .my-masonry-grid_column > div { /* change div to reference your elements you put in <Masonry> */
-                margin-bottom: 30px;
+                margin-bottom: 2rem !important;
+                padding: 15px;
+                padding-bottom: 20px;
                 background-color:#d9ecf1;
-                border-radius: 10px;
+                border-radius: 5px;
                 overflow:hidden;
+                transition: 1s;
+              }
+              .my-masonry-grid_column > div:hover {
+                opacity: 90%;
+                top: -5px;
+                -webkit-box-shadow: 0px 4px 10px 7px rgba(0,0,0,0.28); 
+                box-shadow: 0px 4px 10px 7px rgba(0,0,0,0.28);
+              }
+              .title {
+                font-family: 'Futura','Avenir Next', 'Nunito', 'Arial';
+                padding: 5rem;
+                font-size: 2.5rem;
               }`}</style>
+              <div className="title">
               <h1 style={{
-                  textAlign:"center"
-              }}>Welcome to the Zephyr</h1>
+              }}>Welcome to the Zephyrbook!</h1>
+              <p>Learn more at <a href="https://zephyr.hackclub.com">zephyr.hackclub.com</a></p>
+              </div>
     <Masonry
   breakpointCols={3}
   className="my-masonry-grid"
