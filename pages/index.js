47472d6b22fa0f5abb8c10d9372572c18a1dff4c
Neel Redkar <neel.redkar@gmail.com>
2021-07-27 17:13:41 -0700

    responsive

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import files from "../util/pub"
 import Image from "next/image"
 import Masonry from 'react-masonry-css'
+import Head from "next/head"
 
 const pee = (props) => {
     return (
         <div>
+            <Head>
+<title>Zephyrbook</title>
+<meta name="title" content="Zephyrbook"/>
+<meta name="description" content="A virtual scrapbook website of the #zephyrbook channel!"/>
+
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://zephyrbook.vercel.app"/>
+<meta property="og:title" content="Zephyrbook"/>
+<meta property="og:description" content="A virtual scrapbook website of the #zephyrbook channel!"/>
+<meta property="og:image" content="https://zephyrbook.vercel.app/zephyrbook.png"/>
+
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://zephyrbook.vercel.app"/>
+<meta property="twitter:title" content="Zephyrbook"/>
+<meta property="twitter:description" content="A virtual scrapbook website of the #zephyrbook channel!"/>
+<meta property="twitter:image" content="https://zephyrbook.vercel.app/zephyrbook.png"/>
+            </Head>
             <style jsx global>{`
             .my-masonry-grid {
                 display: -webkit-box; /* Not needed if autoprefixing */
@@ -30,6 +48,7 @@ const pee = (props) => {
                 transition: 1s;
               }
               .my-masonry-grid_column > div:hover {
+                cursor:pointer;
                 opacity: 90%;
                 top: -5px;
                 -webkit-box-shadow: 0px 4px 10px 7px rgba(0,0,0,0.28); 
@@ -37,16 +56,21 @@ const pee = (props) => {
               }
               .title {
                 font-family: 'Futura','Avenir Next', 'Nunito', 'Arial';
-                padding: 5rem;
-                font-size: 2.5rem;
-              }`}</style>
+                padding: 5vw;
+              }
+              `}</style>
               <div className="title">
               <h1 style={{
               }}>Welcome to the Zephyrbook!</h1>
               <p>Learn more at <a href="https://zephyr.hackclub.com">zephyr.hackclub.com</a></p>
               </div>
     <Masonry
-  breakpointCols={3}
+  breakpointCols={{
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  }}
   className="my-masonry-grid"
   columnClassName="my-masonry-grid_column">
   {files.map(v => <Image  src={v.link} width={v.width} height={v.height}/>)}
